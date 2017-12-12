9556885b9d4d7deb54596ab8919d3053f6ce79cc
cynthia1171 <c.isla1171@gmail.com>
2017-12-11 22:00:18 -0300

    caja de contactos con informacion terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,39 @@
+var idContacto = 0;
 $(document).ready(function () {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 });
+
+function saveContact(){
+  var name = $('#name').val();
+  var telefono = $('#telefono').val();
+
+  
+
+  $('#contactos').append('<div class="col m12 contact_' + idContacto +'">'+
+                          '<div class="container">'+
+                            '<div class="card horizontal margenAlto">'+
+                              '<div class="card-stacked">'+
+                                '<div class="card-content">'+
+                                  '<h3>' + name + '</h3>' +
+                                  '<p>' + telefono + '</p>' +
+                                  '<button type="submit" class="waves-effect waves-light btn right btn-top"><i class="material-icons btn-eliminar">delete</i></button>' +
+                                '</div>'+
+                              '</div>'+
+                            '</div>'+
+                          '</div>'+  
+                        '</div>'
+  );
+  $('.btn-eliminar').click(function () {
+    alert('contact_' + idContacto);
+  });
+
+  var name = $('#name').val('');
+  var telefono = $('#telefono').val('');
+  $('#modal1').modal('close');
+  idContacto++;  
+}
+
+
+
+
